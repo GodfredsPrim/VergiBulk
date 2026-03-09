4e3286bb88f7153d8bca61c9978a50a9e23cfa63
Godfreds Bio Conquest <godfredbio2004@gmail.com>
2026-03-09 15:40:25 +0000

    Use environment variable for server port
    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -640,4 +640,5 @@ def catch_all(path):
     return send_from_directory("templates", "index .html")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=DEBUG_MODE)
